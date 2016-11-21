ea9c09942851b89d9a2a40495de5228985e5fa72
Christian Engvall <crilleengvall@gmail.com>
2016-11-21 20:32:33 +0100

    Adding taxonomies

--- custom-post-types/project-custom-post-type.php
+++ custom-post-types/project-custom-post-type.php
@@ -17,3 +17,60 @@ function dpp_setup_post_type() {
 }
 
 add_action( 'init', 'dpp_setup_post_type');
+
+function dpp_setup_post_type_taxonomies() {
+    register_taxonomy(
+        'dpp_languages',
+        'dpp_project',
+        array(
+            'hierarchical' => false,
+            'label' => 'Programming Languages',
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'languages',
+                'with_front' => false
+            )
+        )
+    );
+	register_taxonomy(
+        'dpp_tools',
+        'dpp_project',
+        array(
+            'hierarchical' => false,
+            'label' => 'Tools & Technologies',
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'tools-technologies',
+                'with_front' => false
+            )
+        )
+    );
+	register_taxonomy(
+        'dpp_platforms',
+        'dpp_project',
+        array(
+            'hierarchical' => false,
+            'label' => 'Platforms',
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'platform',
+                'with_front' => false
+            )
+        )
+    );
+
+    register_taxonomy(
+          'dpp_customers',
+          'dpp_project',
+          array(
+              'hierarchical' => false,
+              'label' => 'Customer',
+              'query_var' => true,
+              'rewrite' => array(
+                  'slug' => 'platform',
+                  'with_front' => false
+              )
+          )
+      );
+}
+add_action( 'init', 'dpp_setup_post_type_taxonomies');
