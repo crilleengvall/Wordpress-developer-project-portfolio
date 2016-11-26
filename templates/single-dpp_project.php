833d50667cfb37c08b295f8b08ecda6f0664dc4e
Christian Engvall <crilleengvall@gmail.com>
2016-11-26 17:06:02 +0100

    Showing title and short description above image

--- templates/single-dpp_project.php
+++ templates/single-dpp_project.php
@@ -10,14 +10,11 @@
 
 		<figure>
 			<?php the_post_thumbnail('large'); ?>
+			<figcaption class="dpp-project-short-description">	<?php echo the_title('<h2 itemprop="headline" class="post-title">', '</h2>') . '<div> ' . dpp_project_information_get_meta( 'project_information_short_description' ) .'</div>' ?> </figcaption>
 		</figure>
 
 		<section class="post-section post-body">
 			<div class="wrapper post-wrapper">
-				<?php
-					the_title('<h2 itemprop="headline" class="post-title"><a itemprop="url" href="'. esc_url(get_permalink()) .'" title="'. esc_attr(get_the_title()) .'">', '</a></h2>');
-					print dpp_project_information_get_meta( 'project_information_short_description' );
-				?>
 				<div class="description">
 					<?php print get_the_content(); ?>
 				</div>
