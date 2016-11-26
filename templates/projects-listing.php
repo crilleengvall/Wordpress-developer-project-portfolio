3cf6fefd1456f699abe479826616c9f8002e49ec
Christian Engvall <crilleengvall@gmail.com>
2016-11-26 12:06:55 +0100

    Adding title before short description

--- templates/projects-listing.php
+++ templates/projects-listing.php
@@ -5,7 +5,7 @@
        <a href="<?php echo get_post_permalink($project->ID)?>">
          <?php  echo wp_get_attachment_image( get_post_thumbnail_id( $project->ID ), 'single-post-thumbnail' ); ?>
        </a>
-       <figcaption class="dpp-project-short-description">	<?php echo dpp_project_information_get_meta( 'project_information_short_description', $project->ID ); ?> </figcaption>
+       <figcaption class="dpp-project-short-description">	<?php echo $project->post_title .' - ' . dpp_project_information_get_meta( 'project_information_short_description', $project->ID ); ?> </figcaption>
      </figure>
    </article>
  <?php } ?>
